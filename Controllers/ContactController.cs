d630be3c8038ff2d315bab80902f6e8982d5822f
NathanManiezPro <Nathanmaniez@gmail.com>
2025-01-13 16:48:17 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExerciceASP.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET : /Contact/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET : /Contact/Details
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        // GET : /Contact/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+    
+}
+}
